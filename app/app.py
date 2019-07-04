6d2c046a17399c44744a5e1bf417e83b6fc9e6db
Dongpyeong Seo <bludrimjr@gmail.com>
2019-07-04 16:22:59 -0700

    Add basic routing using Flask

--- app/app.py
+++ app/app.py
@@ -1,1 +1,16 @@
-# test
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello, world!"
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) > 1:
+        port = int(argv[1])
+    else:
+        port = 8080
+    app.run(debug=False, host="0.0.0.0", port=port)
