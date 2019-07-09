2e4648302d16c90a4ee64f98eed9507026458577
Dongpyeong Seo <bludrimjr@gmail.com>
2019-07-09 01:03:57 -0700

    Add a router for retrieving entire workout programs (But not real programs from DB yet)

--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import pickle
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "hello, world!"
+
+@app.route("/api/programs")
+def program_api():
+    if request.method == "GET":
+        with open("program_dict.pickle", "rb") as fp:
+            prog_dict = pickle.load(fp)
+        
+        # prog_dict should be replaced with acceptable dictionary example for workout programs...
+        return jsonify([prog_dict])
+    else:
+        return "Not implemented"
 
 if __name__ == "__main__":
     from sys import argv
