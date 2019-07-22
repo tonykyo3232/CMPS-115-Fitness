6693a8b20a7205c097f62edc4704ad7197aadb6f
unknown <crabiyan@ucsc.edu>
2019-07-21 17:30:54 -0700

    updated parameter for overview() routing

--- app/app.py
+++ app/app.py
@@ -105,9 +105,9 @@ def overview():
 			"cycles": workout_input["cycles"],
 			"is_routine": is_routine
 		}
-		return render_template("overview.jinja", program=sdm.programs)
+		return render_template("overview.jinja", program=workout_input)
 	else:
-		return render_template("overview.jinja", program=sdm.programs)
+		return render_template("overview.jinja", program=workout_input)
 
 @app.route("/program/detail/<int:program_id>", methods=["GET"])
 def program_detail(program_id):
